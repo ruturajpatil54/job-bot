74dec1afe0e774c022f4fda168771cd0331cc46e
Ruturaj Patil <Ruturaj Patil>
2016-05-15 02:31:54 +0530

    test msg

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     
 	@RequestMapping(method=RequestMethod.POST, value="/webhook")
     public String index(@RequestBody String requestJson ) {
-        return "{\"data\":{\"facebook\":{\"recipient\":{\"id\":\"1348601985160132\"},\"message\":{\"attachment\":{\"type\":\"template\",\"payload\":{\"template_type\":\"button\",\"text\":\"What do you want to do next?\",\"buttons\":[{\"type\":\"web_url\",\"url\":\"https://petersapparel.parseapp.com\",\"title\":\"Show Website\"},{\"type\":\"postback\",\"title\":\"Start Chatting\",\"payload\":\"USER_DEFINED_PAYLOAD\"}]}}}}}}";
+        return "{\"data\":{\"facebook\":{\"recipient\":{\"id\":\"1348601985160132\"},\"message\":{\"attachment\":{\"type\":\"template\",\"payload\":{\"template_type\":\"button\",\"text\":\"What do you want to do next?\",\"buttons\":[{\"type\":\"web_url\",\"url\":\"https://www.google.com\",\"title\":\"Show Website\"},{\"type\":\"postback\",\"title\":\"Start Chatting\",\"payload\":\"USER_DEFINED_PAYLOAD\"}]}}}}}}";
     }
     
 }
