a5a31a73b4e942a239f0f4395e10bf42f244b63a
Ruturaj Patil <Ruturaj Patil>
2016-05-15 02:59:57 +0530

    test msg

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 	@RequestMapping(method=RequestMethod.POST, value="/webhook")
     public String index(@RequestBody String requestJson ) {
 			System.out.println("Request:"+requestJson);
-		return "{\"recipient\":{\"id\":\"1348601985160132\"},\"message\":{\"text\":\"hello, world!\"}}";
+		return "{\"data\":{\"facebook\":{\"recipient\":{\"id\":\"1348601985160132\"},\"message\":{\"text\":\"hello, world!\"}}}}";
 //        return "{\"data\":{\"facebook\":{\"recipient\":{\"id\":1348601985160132},\"message\":{\"attachment\":{\"type\":\"template\",\"payload\":{\"template_type\":\"button\",\"text\":\"What do you want to do next?\",\"buttons\":[{\"type\":\"web_url\",\"url\":\"https://www.google.com\",\"title\":\"Show Website\"},{\"type\":\"postback\",\"title\":\"Start Chatting\",\"payload\":\"USER_DEFINED_PAYLOAD\"}]}}}}}}";
     }
     
